c433cf45e7e5969d1738bf10e188fe7b00efcbe5
Johannes M. Schmitt <schmittjoh@gmail.com>
2013-01-01 12:01:47 +0100

    fixes Twig's plus operator on numeric strings (closes #48)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,27 +4,5 @@ if (is_file($file = __DIR__.'/../config.php')) {
     require_once $file;
 }
 
-spl_autoload_register(function($class) {
-    if (0 === strpos($class, 'TwigJs\\Tests\\')) {
-        $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-        if (is_file($file)) {
-            require $file;
+require_once __DIR__.'/../vendor/autoload.php';
 
-            return true;
-        }
-    } elseif (0 === strpos($class, 'TwigJs\\')) {
-        $file = __DIR__ . '/../src/' . str_replace('\\', '/', $class) . '.php';
-        if (is_file($file)) {
-            require $file;
-
-            return true;
-        }
-    } elseif (isset($_SERVER['TWIG_LIB']) && 0 === strpos($class, 'Twig_')) {
-        $file = $_SERVER['TWIG_LIB'] . '/' . str_replace('_', '/', $class) . '.php';
-        if (is_file($file)) {
-            require $file;
-
-            return true;
-        }
-    }
-});

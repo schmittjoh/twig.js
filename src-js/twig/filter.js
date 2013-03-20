5481847bd02a902e5e79dc783421e10f66d67223
Henry Smith <henry@henrysmith.org>
2013-03-20 17:58:55 +0000

    Inline the escapedKey var inside the for loop so the compiler can optimise it

--- src-js/twig/filter.js
+++ src-js/twig/filter.js
@@ -29,8 +29,8 @@ goog.require('goog.object');
  * @return {string}
  */
 twig.filter.replace = function(str, map) {
-	var escapedKey;
 	for (var key in map) {
+		var escapedKey;
 		escapedKey = twig.pregQuote(key);
 		str = str.replace(new RegExp(escapedKey, 'g'), map[key]);
 	}

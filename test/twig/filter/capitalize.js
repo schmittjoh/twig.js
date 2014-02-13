baf17dae29c6a27ee47cba23fde828b1c355c3b9
Henry Smith <henry@henrysmith.org>
2014-02-13 19:49:37 +0000

    Replace all capitalize filter tests with Mocha tests

--- test/twig/filter/capitalize.js
+++ test/twig/filter/capitalize.js
@@ -1,5 +1,11 @@
 describe("TwigJs.Filter.Capitalize", function () {
+
   it("capitalizes the first word", function () {
     twig.filter.capitalize({}, 'hello').should.equal('Hello');
   });
+
+  it("does nothing with an empty string", function () {
+    twig.filter.capitalize({}, '').should.equal('');
+  });
+
 });

99c943cb1c241693ea7c2d5b8cadf62162e7c6b9
Henry Smith <henry@henrysmith.org>
2014-02-13 20:08:44 +0000

    Get rid of the filter test HTML file

--- test/twig/filter/escape.js
+++ test/twig/filter/escape.js
@@ -4,4 +4,16 @@ describe("TwigJs.Filter.Escape", function () {
     twig.filter.escape(null, '<').should.equal('&lt;');
   });
 
+  it("escapes a bracket in HTML mode", function () {
+    twig.filter.escape(null, '<', 'html').should.equal('&lt;');
+  });
+
+  it("escapes JS", function () {
+    twig.filter.escape(null, "\u00dcberpr\u00fcfung", "js").should.equal("\\xDCberpr\\xFCfung");
+  });
+
+  it("escapes URLs", function () {
+    twig.filter.escape(null, "~!@#$%^&*()=+[]{}\\;:\'\",/?", "url").should.equal("~!%40%23%24%25%5E%26*()%3D%2B%5B%5D%7B%7D%5C%3B%3A'%22%2C%2F%3F");
+  });
+
 });

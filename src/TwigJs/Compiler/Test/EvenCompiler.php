acde3683bcf43f452c52c14c7c8fe948fb4654a0
Henry Smith <henry@henrysmith.org>
2014-06-14 15:58:00 +0100

    Add parentheses around the output of the EvenCompiler
    This fixes schmittjoh/twig.js#80. The combination of this polarity check and the
    "not" operator was triggering an operator precedence bug whose end result was
    that the expression "1 is not even" evaluated to false.

--- src/TwigJs/Compiler/Test/EvenCompiler.php
+++ src/TwigJs/Compiler/Test/EvenCompiler.php
@@ -15,9 +15,9 @@ class EvenCompiler implements TestCompilerInterface
     public function compile(JsCompiler $compiler, \Twig_Node_Expression_Test $node)
     {
         $compiler
-            ->raw('0 === ')
+            ->raw('(0 === ')
             ->subcompile($node->getNode('node'))
-            ->raw(' % 2')
+            ->raw(' % 2)')
         ;
     }
 }
